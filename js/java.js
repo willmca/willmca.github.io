c76586861645697c3998e53aa254883a71eba95c
Will McAllister <willmca32@gmail.com>
2019-08-30 15:09:46 -0400

    Added project one

--- js/java.js
+++ js/java.js
@@ -1,10 +1,11 @@
-const links = document.querySelectorAll("a")
+/*const links = document.querySelectorAll("a")
 
 for (i=0;i<links.length;i++){
 links[i].addEventListener("mouseover", function(evt){
     evt.preventDefault();
     console.log(evt)
     evt.target.style.color= "salmon";
+    evt.target.style.textdecoration="underline";
 })
 links[i].addEventListener("mouseout", function(evt){
     evt.preventDefault();
@@ -12,3 +13,4 @@ links[i].addEventListener("mouseout", function(evt){
     evt.target.style.color= "white";
 })
 }
+*/
